cba9a35feedce858014caae8fd7d918ead3c9883
Thu Nguyen <thu.nguyen@talentadore.com>
2019-01-03 08:47:28 +0200

    adjustments

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 from Analytics import create_app
 
-def get_user(current_user):
-    app = create_app('../config.py')
-    if app.config["PRODUCTION"] is False:
-        app.run(debug=True, port=5005)
-    else:
-        app.run()
-
-get_user()
+app = create_app('../config.py')
+if app.config["PRODUCTION"] is False:
+    app.run(debug=True, port=5005)
+else:
+    app.run()
